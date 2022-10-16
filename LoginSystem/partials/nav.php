a59dad1ce1a0d6d279da52dffa50852f7e59a2d7
SREEUDAY <93709464+SREEUDAY23@users.noreply.github.com>
2022-10-16 20:11:32 +0530

    Add files via upload

---
+++ LoginSystem/partials/nav.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/LoginSystem">isecure</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/LoginSystem/welcome.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/LoginSystem/login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/LoginSystem/signup.php">Sign Up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/LoginSystem/logout.php">Log Out</a>
+      </li>
+      
+      
+    </ul>
+    
+  </div>
+</nav>
